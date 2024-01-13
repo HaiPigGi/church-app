23eaacdbb7c7101fae8877767797d9722cc6b260
jepan <jheffanD.com>
2024-01-13 12:18:58 +0700

    modified:   next-app/src/app/pages/login/page.jsx 	modified:   next-app/src/app/pages/register/page.jsx 	modified:   next-app/tailwind.config.js

--- next-app/tailwind.config.js
+++ next-app/tailwind.config.js
@@ -7,6 +7,9 @@ module.exports = {
   ],
   theme: {
     extend: {
+      spacing: {
+        '100': '32rem',
+      },
       backgroundImage: {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
         'gradient-conic':
@@ -26,7 +29,6 @@ module.exports = {
         'custom': '#FF9843', 
         'custom2': '#ffffff66', 
       },
-    
     },
   },
   plugins: [],
