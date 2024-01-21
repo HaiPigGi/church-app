91bd119175614d35518ea62058ecca12e975e00d
abdisavia <ridhodanabdi7a@gmail.com>
2024-01-21 23:20:28 +0700

    new update

--- next-app/src/app/page.jsx
+++ next-app/src/app/page.jsx
@@ -1,28 +1,17 @@
 'use client';
 import PageHome from './pages/home/page';
-import { useEffect,useState } from 'react';
+import { useEffect } from 'react';
 import Loading from '@/components/Fragments/Loading/loading';
 import { useAppSelector } from '@/lib/hook';
 
 export default function Home() {
   //get the AuthStatus
-  const AuthStatus = useAppSelector((state) => state.session.status);
-  const [displayLoading, setDisplayLoading] = useState(true);
 
   //execute function Load whenever the AuthStatus changed
-  useEffect(() => {
-    const timeoutId = setTimeout(() => {
-      setDisplayLoading(false);
-    }, 2000); // Set waktu yang diinginkan (dalam milidetik)
-
-    // Membersihkan timer saat komponen dibongkar (unmounted)
-    return () => clearTimeout(timeoutId);
-  }, [AuthStatus]);
-
-  // function for Loading
+  const AuthStatus = useAppSelector((state) => state.session.status);
   const Load = () => {
-    if (displayLoading) {
-      // Menampilkan animasi loading selama 3 detik
+    if (AuthStatus == 'loading') {
+      // change the loading animation in components/fragments/Loading
       return <Loading />;
     }
     return <PageHome />;
@@ -30,7 +19,7 @@ export default function Home() {
 
   useEffect(() => {
     Load();
-  }, [AuthStatus]);
+  }, [AuthStatus, Load]);
 
   // function for Loading
 
