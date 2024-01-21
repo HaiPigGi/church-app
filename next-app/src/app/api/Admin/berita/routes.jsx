e94fdb7c9c5add79f0fa8b3a92c15ed8dfb0b77f
abdisavia <ridhodanabdi7a@gmail.com>
2024-01-21 23:09:58 +0700

    new update

--- next-app/src/app/api/Admin/berita/routes.jsx
+++ next-app/src/app/api/Admin/berita/routes.jsx
@@ -1,110 +1,118 @@
-const beritaServices = () => {
-  const token = sessionStorage.getItem('jwtToken');
-  async function get_AllBerita() {
-    try {
-      const res = await fetch(
-        `${process.env.NEXT_PUBLIC_API_DOMAIN}/api/admin/berita/`,
-        {
-          method: 'GET',
-          mode: 'cors',
-          headers: {
-            'content-type': 'application/json',
-            Authorization: `bearer ${token}`,
-          },
-        },
-      );
-      const responseData = await res.json();
-      if (res.status == 200) {
-        return responseData;
-      }
+// To get the JWTTOKEN from session storage.
+function getJwtToken() {
+  return sessionStorage.getItem('jwtToken');
+}
 
-      console.log(responseData.error);
-      return;
-    } catch (e) {
-      console.log('error in get_AllBerita with message : ', e.message);
-    }
-  }
-
-  async function get_beritaID(beritaID) {
-    try {
-      const res = fetch(
-        `${process.env.NEXT_PUBLIC_API_DOMAIN}/api/admin/berita/${id}`,
-        {
-          method: 'GET',
-          mode: 'cors',
-          headers: {
-            'content-type': 'application/json',
-            Authorization: `bearer ${token}`,
-          },
+// to get all data berita from server
+export async function get_AllBerita() {
+  try {
+    const res = await fetch(
+      `${process.env.NEXT_PUBLIC_API_DOMAIN}/api/admin/berita/`,
+      {
+        method: 'GET',
+        mode: 'cors',
+        headers: {
+          'content-type': 'application/json',
+          Authorization: `bearer ${getJwtToken()}`,
         },
-      );
-      const responseData = await res.json();
-      if (res.status == 200) {
-        return responseData;
-      }
-      console.log(responseData.error);
-      return;
-    } catch (e) {
-      console.log('error in get_beritaID with message : ', e.message);
-    }
-  }
-
-  async function post_berita(dataPost) {
-    try {
-      const res = fetch(
-        `${process.env.NEXT_PUBLIC_API_DOMAIN}/api/admin/berita/store`,
-        {
-          method: 'POST',
-          mode: 'cors',
-          headers: {
-            Authorization: `bearer ${token}`,
-          },
-          body: JSON.stringify(dataPost),
-        },
-      );
-      const responseData = await res.json();
+      },
+    );
+    const responseData = await res.json();
+    if (res.status == 200) {
       return responseData;
-    } catch (e) {
-      console.log('error in put_berita with message : ', e.message);
     }
-  }
 
-  async function put_berita(dataPost) {
-    try {
-      const res = fetch(
-        `${process.env.NEXT_PUBLIC_API_DOMAIN}/api/admin/berita/${dataPost.id}`,
-        {
-          method: 'PUT',
-          mode: 'cors',
-          headers: {
-            Authorization: `bearer ${token}`,
-          },
-          body: JSON.stringify(dataPost),
-        },
-      );
-      const responseData = await res.json();
-      return responseData;
-    } catch (e) {
-      console.log('error in put_berita with message : ', e.message);
-    }
+    console.log(responseData.error);
+    return;
+  } catch (e) {
+    console.log('error in get_AllBerita with message : ', e.message);
   }
+}
 
-  async function delete_berita(beritaID) {
-    try {
-      const res = fetch(
-        `${process.env.NEXT_PUBLIC_API_DOMAIN}/api/admin/berita/${beritaID}`,
-        {
-          method: 'DELETE',
-          mode: 'cors',
-          headers: {
-            'X-CSRF-TOKEN': csrf_token,
-          },
+// to get berita based on id
+export async function get_beritaID(beritaID) {
+  try {
+    const res = fetch(
+      `${process.env.NEXT_PUBLIC_API_DOMAIN}/api/admin/berita/${id}`,
+      {
+        method: 'GET',
+        mode: 'cors',
+        headers: {
+          'content-type': 'application/json',
+          Authorization: `bearer ${getJwtToken()}`,
         },
-      );
-      const responseData = await res.json();
+      },
+    );
+    const responseData = await res.json();
+    if (res.status == 200) {
       return responseData;
-    } catch (e) {
-      console.log('error in delete_berita with message : ', e.message);
     }
+    console.log(responseData.error);
+    return;
+  } catch (e) {
+    console.log('error in get_beritaID with message : ', e.message);
   }
-};
+}
+
+// to post new berita
+export async function post_berita(dataPost) {
+  try {
+    const res = fetch(
+      `${process.env.NEXT_PUBLIC_API_DOMAIN}/api/admin/berita/store`,
+      {
+        method: 'POST',
+        mode: 'cors',
+        headers: {
+          Authorization: `bearer ${getJwtToken()}`,
+        },
+        body: JSON.stringify(dataPost),
+      },
+    );
+    const responseData = await res.json();
+    return responseData;
+  } catch (e) {
+    console.log('error in put_berita with message : ', e.message);
+  }
+}
+
+// to update bertia based on id berita
+export async function put_berita(dataPost) {
+  try {
+    const res = fetch(
+      `${process.env.NEXT_PUBLIC_API_DOMAIN}/api/admin/berita/${dataPost.id}`,
+      {
+        method: 'PUT',
+        mode: 'cors',
+        headers: {
+          Authorization: `bearer ${getJwtToken()}`,
+        },
+        body: JSON.stringify(dataPost),
+      },
+    );
+    const responseData = await res.json();
+    return responseData;
+  } catch (e) {
+    console.log('error in put_berita with message : ', e.message);
+  }
+}
+
+// to delete berita based on id berita
+export async function delete_berita(beritaID) {
+  //
+  try {
+    const res = fetch(
+      `${process.env.NEXT_PUBLIC_API_DOMAIN}/api/admin/berita/${beritaID}`,
+      {
+        method: 'DELETE',
+        mode: 'cors',
+        headers: {
+          'X-CSRF-TOKEN': csrf_token,
+        },
+      },
+    );
+    const responseData = await res.json();
+    return responseData;
+  } catch (e) {
+    console.log('error in delete_berita with message : ', e.message);
+  }
+}
--- next-app/src/lib/features/Berita/beritaSlice.js
+++
@@ -1,7 +0,0 @@
-import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
-
-export default const beritaSlice = createSlice({
-name: "Berita",
-    initialState,
-
-})
