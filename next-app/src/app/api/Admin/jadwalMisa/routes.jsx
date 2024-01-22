3942c4122affd04e4b42edc6d08682cc60750291
abdisavia <ridhodanabdi7a@gmail.com>
2024-01-22 21:06:34 +0700

    new update

--- next-app/src/app/api/Admin/jadwalMisa/routes.jsx
+++ next-app/src/app/api/Admin/jadwalMisa/routes.jsx
@@ -1,1 +1,116 @@
-export async function getAllOrganitation() {}
+const token = sessionStorage.getItem('jwtToken');
+export async function get_JadwalMisa() {
+  try {
+    const res = await fetch(
+      `${process.env.NEXT_PUBLIC_API_DOMAIN}/api/admin/jadwal-misa/`,
+      {
+        method: 'GET',
+        mode: 'cors',
+        headers: {
+          'content-type': 'application/json',
+          Authorization: `bearer ${token}`,
+        },
+      },
+    );
+    if (res.ok) {
+      const responseData = await res.json();
+      return responseData;
+    }
+  } catch (e) {
+    console.log('error at getJadwalMisa with message : ', e.message);
+  }
+}
+
+export async function get_JadwalMisaid(jadwalID) {
+  try {
+    const res = await fetch(
+      `${process.env.NEXT_PUBLIC_API_DOMAIN}/api/admin/jadwal-misa/${jadwalID}`,
+      {
+        method: 'GET',
+        mode: 'cors',
+        headers: {
+          'content-type': 'application/json',
+          Authorization: `bearer ${token}`,
+        },
+      },
+    );
+    const responseData = await res.json();
+    if (res.ok) {
+      return responseData;
+    }
+    console.log(
+      'response error at get_JadwalMisaid with message : ',
+      responseData.error,
+    );
+  } catch (e) {
+    console.log('error at get_JadwalMisaid with message : ', e.message);
+  }
+}
+
+export async function post_JadwalMisa(dataJadwal) {
+  try {
+    const res = await fetch(
+      `${process.env.NEXT_PUBLIC_API_DOMAIN}/api/admin/jadwal-misa/`,
+      {
+        method: 'POST',
+        mode: 'cors',
+        headers: {
+          Authorization: `bearer ${token}`,
+        },
+        body: JSON.stringify(dataJadwal),
+      },
+    );
+    const responseData = await res.json();
+    if (res.ok) {
+      return responseData.message;
+    }
+    return responseData.error;
+  } catch (e) {
+    console.log('Error at post_JadwalMisa with message : ', e.message);
+  }
+}
+
+export async function put_JadwalMisa(jadwalID, dataJadwal) {
+  try {
+    const res = await fetch(
+      `${process.env.NEXT_PUBLIC_API_DOMAIN}/api/admin/jadwal-misa/${jadwalID}`,
+      {
+        method: 'PUT',
+        mode: 'cors',
+        headers: {
+          Authorization: `bearer ${token}`,
+        },
+        body: JSON.stringify(dataJadwal),
+      },
+    );
+    const responseData = await res.json();
+    if (res.ok) {
+      return responseData.message;
+    }
+    return responseData.error;
+  } catch (e) {
+    console.log('error at put_JadwalMisa with message : ', e.message);
+  }
+}
+
+export async function delete_JadwalMisa(jadwalID) {
+  try {
+    const res = await fetch(
+      `${process.env.NEXT_PUBLIC_API_DOMAIN}/api/admin/jadwal-misa/${jadwalID}`,
+      {
+        method: 'DELETE',
+        mode: 'cors',
+        headers: {
+          Authorization: `bearer ${token}`,
+        },
+      },
+    );
+    const responseData = await res.json();
+    if (res.ok) {
+      return responseData;
+    }
+    return responseData;
+  } catch (e) {
+    console.log('error at delete_JadwalMisa with message : ', e.message);
+  }
+}
