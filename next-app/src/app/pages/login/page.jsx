71947551bd2bbe1a1f859b51a38450c602a13ee8
abdisavia <ridhodanabdi7a@gmail.com>
2024-02-06 00:13:24 +0700

    fix : fixing mobile view

--- next-app/src/app/pages/login/page.jsx
+++ next-app/src/app/pages/login/page.jsx
@@ -87,31 +87,36 @@ export default function Login() {
     if (openModal == true) {
       return 'blur-sm h-screen w-full bg-hero bg-fixed bg-center bg-cover bg-no-repeat overflow-hidden';
     }
-    return 'h-screen w-full bg-hero bg-fixed bg-center bg-cover bg-no-repeat overflow-hidden';
+    return 'h-screen w-full overflow-hidden grid md:grid-cols-3 grid-row-4 p-2 md:p-0';
   };
 
   return (
     <MainLayout>
       <Navbar />
       <section className={clsSection()}>
-        <div className="snap-always snap-start w-full h-screen absolute flex justify-center items-center right-[15rem]">
-          <div id="content-Hero" className="">
-            <h1 className="text-4xl text-shadow font-bold text-white text-center">
+        <div className="w-full row-span-2 md:col-span-2 md:h-screen h-full relative flex justify-center items-center">
+          <div id="content-Hero" className="absolute z-20">
+            <h1 className="text-2xl md:text-4xl text-shadow font-bold text-white text-center">
               Gereja <span className="block text-white">ST. Markus Melak</span>
             </h1>
-            <p className="text-center text-white font-light text-sm text-shadow my-2">
-              Temukan lebih banyak tentang komunitas gereja kami <br />
-              dengan login sekarang untuk eksplorasi lengkap
+            <p className="text-center w-80 text-white font-light text-sm text-shadow my-2">
+              Temukan lebih banyak tentang komunitas gereja kami dengan login
+              sekarang untuk eksplorasi lengkap
             </p>
           </div>
+          <div className="absolute w-full h-full left-0 top-0 bg-hero bg-center bg-cover bg-no-repeat rounded-xl"></div>
         </div>
 
-        <div className="bg-white p-4 shadow-md overflow-hidden w-[35rem] h-[100vh] fixed top-0 pt-2 right-0">
-          <section className="bg-white">
-            <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto lg:py-20">
-              <div className="w-full bg-white rounded-lg shadow dark:border md:mt-0  sm:max-w-md xl:p-0 min-[360px]:max-[555px]:w-[22rem] min-[360px]:max-[555px]:ml-[12.5rem]">
-                <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
-                  <h1 className="text-xl font-bold leading-tight tracking-tight text-yellow-800 md:text-2xl ">
+        <div className="row-span-1 md:col-span-1 bg-white relative p-4 shadow-md w-full h-full md:h-screen flex items-start md:items-center">
+          <section className=" w-full ">
+            <div className="flex items-center justify-center  mx-auto w-full h-full">
+              <form
+                className="w-full bg-white rounded-lg shadow dark:border md:mt-0  "
+                action={handleClickLogin}
+                method="post"
+              >
+                <div className="p-6 space-y-4 md:space-y-6  w-full">
+                  <h1 className="text-xl font-bold leading-tight tracking-tight text-yellow-800 md:text-2xl w-full">
                     Sign in to your account
                   </h1>
                   {errorMessage != '' ? (
@@ -121,31 +126,28 @@ export default function Login() {
                   ) : (
                     ''
                   )}
-                  <form
-                    className="space-y-4 md:space-y-6"
-                    action={handleClickLogin}
-                  >
-                    <div>
+                  <div className="space-y-4 md:space-y-6 w-full ">
+                    <div className="w-full">
                       <input
                         type="text"
                         name="name"
                         onChange={handleChange}
-                        className="bg-gray-50 border border-yellow-800 text-primary rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 :placeholder-gray-400 min-[360px]:max-[555px]:w-72"
+                        className="bg-gray-50 border border-yellow-800 text-primary rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 :placeholder-gray-400"
                         placeholder="username"
                         required
                       />
                     </div>
-                    <div>
+                    <div className="w-full">
                       <input
                         name="password"
                         type="password"
-                        className="bg-gray-50 border mb-2 border-yellow-800 text-primary rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 :placeholder-gray-400 min-[360px]:max-[555px]:w-72"
+                        className="bg-gray-50 border mb-2 border-yellow-800 text-primary rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 :placeholder-gray-400"
                         placeholder="password"
                         onChange={handleChange}
                         required
                       />
                     </div>
-                    <div className="flex items-center justify-between">
+                    <div className="flex items-center justify-between w-full">
                       <a
                         href="#"
                         className="text-sm font-medium text-primary-600 hover:underline dark:text-primary-500 mt-3 "
@@ -155,7 +157,7 @@ export default function Login() {
                     </div>
                     <button
                       type="submit"
-                      className="w-full text-white bg-primary focus:ring-4 focus:outline-none  font-medium rounded-lg text-sm px-5 py-2.5 text-center  min-[360px]:max-[555px]:w-72"
+                      className="w-full text-white bg-primary focus:ring-4 focus:outline-none  font-medium rounded-lg text-sm px-5 py-2.5 text-center  "
                     >
                       Login
                     </button>
@@ -168,9 +170,9 @@ export default function Login() {
                         Sign up
                       </a>
                     </p>
-                  </form>
+                  </div>
                 </div>
-              </div>
+              </form>
             </div>
           </section>
         </div>
