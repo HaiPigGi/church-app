a1ad8e5adf955bfdce4ee5ab0a76f417cf599035
abdisavia <ridhodanabdi7a@gmail.com>
2024-02-05 23:15:34 +0700

    change : change the position value to centering the modal

--- next-app/src/components/Fragments/Modal/ModalKonfirmasi.jsx
+++ next-app/src/components/Fragments/Modal/ModalKonfirmasi.jsx
@@ -2,7 +2,7 @@ export default function ModalKonfirmasi({ actionAcc, actionDecline }) {
   return (
     <div
       id="bgModal"
-      className="absolute z-40 top-0 box-border w-full h-full flex justify-center items-center"
+      className="absolute z-40 top-0 left-0 box-border w-full h-full flex justify-center items-center"
     >
       <div className="relative min-w-[350px] max-w-[500px] min-h-[250px] md:min-w-[350px] shadow-xl md:max-w-[500px] p-5 h-auto w-auto bg-white z-50 rounded-xl flex justify-center items-center">
         <div>
