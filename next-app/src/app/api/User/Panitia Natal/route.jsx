c7e25fe01e4bc58251373df85ca7ed174609b2fa
HaiPigGi <leonardobryan32@gmail.com>
2024-02-12 11:06:28 +0800

    handle code for panitia natal and panitia paskah

---
+++ next-app/src/app/api/User/Panitia Natal/route.jsx
@@ -0,0 +1,18 @@
+export const getAllDataPanitiaNatal = async () => {
+    try {
+      const res = await fetch(
+        `${process.env.NEXT_PUBLIC_API_DOMAIN}/api/user/member/`,
+        {
+          method: 'GET',
+          mode: 'cors',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+        },
+      );
+      return res;
+    } catch (e) {
+      console.log('error at getAllDokuemntasi with message : ', e.message);
+    }
+  };
+  
